b692cbb35ec9bdffa1e5c845088b80f6acc3c723
Emilien-T <112722759+Emilien-T@users.noreply.github.com>
2026-05-23 10:44:04 -0400

    doing stuff

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,16 +1,44 @@
+using DG.Tweening;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController : Controller<CameraController>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform zenTransform;
+    [SerializeField] private Transform baseTransform;
+    [SerializeField] private Transform boltTransform;
+    [SerializeField] private Transform buttonTransform;
+    [SerializeField] private Transform nailTransform;
+    [SerializeField] private float transitionTime = 0.7f;
+
+    private void Transition(Transform target)
     {
-        
+        Sequence seq = DOTween.Sequence();
+        seq.Append(transform.DOMove(target.position, transitionTime).SetEase(Ease.InOutQuad));
+        seq.Join(transform.DORotate(target.rotation.eulerAngles, transitionTime).SetEase(Ease.InOutQuad));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToZen()
     {
-        
+        Transition(zenTransform);
+    }
+
+    public void GoToBase()
+    {
+        Transition(baseTransform);
+    }
+
+    public void GoToBolt()
+    {
+        Transition(boltTransform);
+    }
+
+    public void GoToButton()
+    {
+        Transition(buttonTransform);
+    }
+
+    public void GoToNail()
+    {
+        Transition(nailTransform);
     }
 }
