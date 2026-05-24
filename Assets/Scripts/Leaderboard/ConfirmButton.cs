e1ab9873cfb27d8e0a0725e922c68518afb1d2bd
DontDieThankYou <96702893+DontDieThankYou@users.noreply.github.com>
2026-05-24 11:59:34 -0400

    Leaderboard and sounds stuff

---
+++ Assets/Scripts/Leaderboard/ConfirmButton.cs
@@ -0,0 +1,16 @@
+public class ConfirmButton : Letter
+{
+    public override void Start()
+    {
+        letter = ' ';
+    }
+    public override void LetterUp()
+    {
+    }
+
+    public override void LetterDown()
+    {
+    }
+    public override void Activate() { }
+    public override void Deactivate() { }
+}
